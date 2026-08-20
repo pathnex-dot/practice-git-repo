c7801eddea8f5e137d8816e83817c91e914b7e4b
meenakshi bhati <aakash653kumar@gmail.com>
2026-08-20 20:34:31 +0530

    new changes add

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from TeamCity!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("Build successful!");
+    }
+}
